ce769bb6596cf702d4868067e60bdf9756b704f1
Tim Boldt <tim.boldt@gmail.com>
2022-12-09 22:01:40 -0800

    Day 6

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,83 @@
+//  Copyright 2022 Google LLC
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+#![warn(clippy::all)]
 #![no_std]
 #![no_main]
 
-// pick a panicking behavior
-use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-// use panic_abort as _; // requires nightly
-// use panic_itm as _; // logs messages over ITM; requires ITM support
-// use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
-
-use cortex_m::asm;
 use cortex_m_rt::entry;
+use cortex_m_semihosting::{debug, hprintln};
+use panic_halt as _;
+
+fn parse(input: &[u8], output: &mut [u8]) {
+    for (i, ch) in input.iter().enumerate() {
+        output[i] = *ch;
+    }
+}
+
+fn part1(puzzle: &[u8]) -> i32 {
+    let mut marker: [u8; 4] = puzzle[0..4].try_into().unwrap();
+    for (idx, ch) in puzzle.iter().enumerate() {
+        marker[idx % 4] = *ch;
+        let mut dup = false;
+        for i in 0..4 {
+            for j in i + 1..4 {
+                if marker[i] == marker[j] {
+                    dup = true;
+                }
+            }
+        }
+        if !dup {
+            return idx as i32 + 1;
+        }
+    }
+    0
+}
+
+fn part2(puzzle: &[u8]) -> i32 {
+    let mut marker: [u8; 14] = puzzle[0..14].try_into().unwrap();
+    for (idx, ch) in puzzle.iter().enumerate() {
+        marker[idx % 14] = *ch;
+        let mut dup = false;
+        for i in 0..14 {
+            for j in i + 1..14 {
+                if marker[i] == marker[j] {
+                    dup = true;
+                }
+            }
+        }
+        if !dup {
+            return idx as i32 + 1;
+        }
+    }
+    0
+}
 
 #[entry]
 fn main() -> ! {
-    asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
+    let input = include_bytes!("../input/06.txt");
+    // TODO: make static.
+    let mut parsed = [b' '; 8192];
+    parse(input, &mut parsed);
 
-    loop {
-        // your code goes here
-    }
+    let p1 = part1(&parsed);
+    hprintln!("Part 1: {:?}", p1).unwrap();
+
+    let p2 = part2(&parsed);
+    hprintln!("Part 2: {:?}", p2).unwrap();
+
+    // Exit QEMU.
+    debug::exit(debug::EXIT_SUCCESS);
+    loop {}
 }
