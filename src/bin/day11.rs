8bbec4c7508ca618a04e67e0d6d1125e1e1170db
Tim Boldt <tim.boldt@gmail.com>
2022-12-11 20:33:56 -0800

    Day 11 Part 1

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -22,26 +22,31 @@ use cortex_m_semihosting::{debug, hprintln};
 use panic_halt as _;
 use safe_regex;
 
-#[derive(Debug, Clone, Copy)]
+const NUM_MONKEYS: usize = 8;
+const MAX_ITEMS: usize = 32;
+const PARSE_SIZE: usize = 256;
+
+#[derive(Debug, Clone, Copy, Default)]
 struct Monkey {
     num_inspections: usize,
-    items: [usize; 8],
+    items: [usize; MAX_ITEMS],
     op: MonkeyOp,
     modulus: usize,
     if_true: usize,
     if_false: usize,
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, Default)]
 enum MonkeyOp {
     Plus(usize),
     Times(usize),
+    #[default]
     Square,
 }
 
 fn parse_monkey(input: &[u8], monkey: &mut Monkey) {
     // Flatten out the line feeds.
-    let mut flattened = [b' '; 256];
+    let mut flattened = [b' '; PARSE_SIZE];
     for (i, b) in input.iter().enumerate() {
         flattened[i] = match b {
             b'\n' => b' ',
@@ -54,7 +59,7 @@ fn parse_monkey(input: &[u8], monkey: &mut Monkey) {
         re.match_slices(&flattened).unwrap();
     *monkey = Monkey {
         num_inspections: 0,
-        items: [0; 8],
+        items: [0; MAX_ITEMS],
         op: match (operator, operand) {
             (b"+", v) => MonkeyOp::Plus(atoi::<usize>(v).unwrap()),
             (b"*", b"old") => MonkeyOp::Square,
@@ -83,14 +88,13 @@ fn parse_monkey(input: &[u8], monkey: &mut Monkey) {
     }
 }
 
-fn parse(input: &[u8], monkeys: &mut [Monkey; 8]) {
+fn parse(input: &[u8], monkeys: &mut [Monkey; NUM_MONKEYS]) {
     let mut start = 0;
     let mut end = 1;
 
-    // HACK: There are exactly 8 monkeys.
-    for m in 0..8 {
+    for m in 0..NUM_MONKEYS {
         // HACK: Manually split on double linefeed.
-        for i in start + 1..start + 256 {
+        for i in start + 1..start + PARSE_SIZE {
             end = i;
             if i == input.len() {
                 break;
@@ -99,39 +103,68 @@ fn parse(input: &[u8], monkeys: &mut [Monkey; 8]) {
                 break;
             }
         }
-
         parse_monkey(&input[start..end], &mut monkeys[m]);
-        hprintln!("Monkey {}: {:?}", m, monkeys[m]).unwrap();
-    
         start = end + 1;
     }
 }
-/*
-fn part1(monkeys: &[Monkey]) -> usize {
-    let mut monkeys = monkeys.iter().cloned().collect_vec();
+
+fn part1(parsed: &[Monkey]) -> usize {
+    let mut monkeys: [Monkey; NUM_MONKEYS] = Default::default();
+    for m in 0..NUM_MONKEYS {
+        monkeys[m] = parsed[m].clone();
+    }
     for _ in 0..20 {
         for m in 0..monkeys.len() {
-            while let Some(w) = monkeys[m].items.pop() {
-                let worry = match monkeys[m].op {
-                    MonkeyOp::Plus(x) => w + x,
-                    MonkeyOp::Times(x) => w * x,
-                    MonkeyOp::Square => w * w,
-                } / 3;
-                let target = if worry % monkeys[m].modulus == 0 {
-                    monkeys[m].if_true
-                } else {
-                    monkeys[m].if_false
-                };
-                monkeys[target].items.push(worry);
-                monkeys[m].num_inspections += 1;
+            for i in 0..monkeys[m].items.len() {
+                let item = monkeys[m].items.get_mut(i).unwrap();
+                if *item != 0 {
+                    let w = *item;
+                    *item = 0;
+                    let worry = match monkeys[m].op {
+                        MonkeyOp::Plus(x) => w + x,
+                        MonkeyOp::Times(x) => w * x,
+                        MonkeyOp::Square => w * w,
+                    } / 3;
+                    let target = if worry % monkeys[m].modulus == 0 {
+                        monkeys[m].if_true
+                    } else {
+                        monkeys[m].if_false
+                    };
+                    let mut ok = false;
+                    for t in 0..monkeys[target].items.len() {
+                        if monkeys[target].items[t] == 0 {
+                            monkeys[target].items[t] = worry;
+                            ok = true;
+                            break;
+                        }
+                    }
+                    if !ok {
+                        hprintln!("Monkey items overflow!").unwrap();
+                    }
+                    monkeys[m].num_inspections += 1;
+                }
             }
         }
     }
-    // Reverse sort.
-    monkeys.sort_by(|b, a| a.num_inspections.cmp(&b.num_inspections));
-    monkeys.iter().take(2).map(|m| m.num_inspections).product()
+
+    // Find the top two and multiply them together.
+    let mut top_two: [usize; 2] = [0; 2];
+    for m in monkeys {
+        if m.num_inspections > top_two[0] {
+            if top_two[0] > top_two[1] {
+                top_two[1] = top_two[0];
+            }
+            top_two[0] = m.num_inspections;
+            continue;
+        }
+        if m.num_inspections > top_two[1] {
+            top_two[1] = m.num_inspections;
+        }
+    }
+    top_two.iter().product()
 }
 
+/*
 fn part2(monkeys: &[Monkey]) -> usize {
     let mut monkeys = monkeys.iter().cloned().collect_vec();
     let modulus: usize = monkeys.iter().map(|m| m.modulus).product();
@@ -162,18 +195,11 @@ fn part2(monkeys: &[Monkey]) -> usize {
 fn main() -> ! {
     let input = include_bytes!("../../input/11.txt");
 
-    let mut monkeys = &mut [Monkey {
-        num_inspections: 0,
-        items: [0; 8],
-        op: MonkeyOp::Square,
-        modulus: 0,
-        if_true: 0,
-        if_false: 0,
-    }; 8];
+    let mut monkeys: &mut [Monkey; NUM_MONKEYS] = &mut Default::default();
     parse(input, &mut monkeys);
+    let p1 = part1(monkeys);
+    hprintln!("Part 1: {:?}", p1).unwrap();
     /*
-        let p1 = part1(&parsed);
-        hprintln!("Part 1: {:?}", p1).unwrap();
 
         let p2 = part2(&parsed);
         hprintln!("Part 2: {:?}", p2).unwrap();
